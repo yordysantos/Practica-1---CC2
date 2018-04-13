2c33fd8eec612c663f303f0d0bd16624e353a95d
Yordy Santos <yordywsantos@gmail.com>
2018-04-13 11:13:43 -0500

    Update Ejercicio 05.cpp

--- Ejercicio 05.cpp
+++ Ejercicio 05.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 using namespace std;
-int longitud(char *s)
-{
-    return (*s) ? 1 + longitud(s + 1) : 0;
+
+int tam1 (char V[]){
+    int cont = 0;
+    int i=0;
+    //for  (int i=0;V[i]!='\0';i++)
+    while(V[i]!='\0')
+    {
+        i++;
+        cont++;
+    }
+    return cont;
+
 }
-int tamano(char s[])
-{
-	int cont=0;
-	for(int i=0;s[i]!='\0';i++){
-		cont++;
-	}
-	return cont;
+int tam2 (char V[]){
+    char *p1=V;
+    char *p2=V;
+    while (*p2 != '\0'){
+        p2++;
+    }
+    return p2-p1;
 }
-int main(){
-	char *p=new char[100];
-	char cad[100];
-	cout<<"Cadena 1: ";
-	gets(p);
-	cout<<"Cadena 2: ";
-	gets(cad);
-	cout<<"Punteros: "<<longitud(p)<<endl;
-	cout<<"Arreglos: "<<tamano(cad)<<endl;
+
+int main()
+{
+  int s=0,op;
+  while(s==0)
+  {
+    char a[] = "hola mundo";
+    cout<<" --->Usando arreglos"<<endl;
+    cout<<"\t\tEl tamaño de la cadena es: "<<tam1(a)<<endl;
+    cout<<" --->Usando punteros"<<endl;
+    cout << "\t\tEl tamaño de la cadena es: "<<tam2(a)<<endl;
+    cout<<"Para Salir ingrese 1"<<endl;
+    cout<<"Para Continuar ingrese otro numero"<<endl;
+    cout<<"Salir? ";cin>>op;
+    cout<<"----------------------------------------\n\n";
+    if(op==1)
+      break;
+  }
 }
