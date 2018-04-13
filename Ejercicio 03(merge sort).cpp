0f905a6afbb502bfeffecf252a9bb218e09f6903
Yordy Santos <yordywsantos@gmail.com>
2018-04-13 11:17:32 -0500

    Update Ejercicio 03(merge sort).cpp

--- Ejercicio 03(merge sort).cpp
+++ Ejercicio 03(merge sort).cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void merge(int a[], int startIndex, int endIndex)
 {
 
